bfc87e777deaabd7e5dac5033f88cc24b68d2f67
Ahmed Kamel <ahmedkamel.egy5577@gmail.com>
2024-01-25 02:22:45 +0200

    final commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,9 @@ import Button from "./Button";
 
 const Hero = () => {
 	return (
-		<section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
+		<section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
 			<div className="hero-map" />
+
 			<div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
 				<Image
 					src="/camp.svg"
@@ -34,23 +35,26 @@ const Hero = () => {
 								/>
 							))}
 					</div>
+
 					<p className="bold-16 lg:bold-20 text-blue-70">
-						198K{" "}
-						<span className="regular-16 lg-regular-20 ml-1 ">
-							Exellent reviews
+						198k
+						<span className="regular-16 lg:regular-20 ml-1">
+							Excellent Reviews
 						</span>
 					</p>
-					<div className=" flex flex-col w-full gap-3 sm:flex-row">
-						<Button type="button" title="Download Appp" variant="btn_green" />
-						<Button
-							type="button"
-							title="How we work?"
-							variant="btn_white"
-							icon="/play.svg"
-						/>
-					</div>
+				</div>
+
+				<div className="flex flex-col w-full gap-3 sm:flex-row">
+					<Button type="button" title="Download App" variant="btn_green" />
+					<Button
+						type="button"
+						title="How we work?"
+						icon="/play.svg"
+						variant="btn_white_text"
+					/>
 				</div>
 			</div>
+
 			<div className="relative flex flex-1 items-start">
 				<div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
 					<div className="flex flex-col">
