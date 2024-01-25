a4eedafc4e883c94923bd4d8f1d061d643827240
Ahmed Kamel <ahmedkamel.egy5577@gmail.com>
2024-01-25 02:12:47 +0200

    final commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,14 +3,18 @@ import Image from "next/image";
 type ButtonProps = {
 	type: "button" | "submit";
 	title: string;
-	icon: string;
-	varient: string;
+	icon?: string;
+	variant: string;
+	full?: boolean;
 };
-const Button = ({ type, title, icon, varient }: ButtonProps) => {
+
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
 	return (
 		<button
+			className={`flexCenter gap-3 rounded-full border ${variant} ${
+				full && "w-full"
+			}`}
 			type={type}
-			className={`flexCenter gap-3 rounded-full border ${varient}`}
 		>
 			{icon && <Image src={icon} alt={title} width={24} height={24} />}
 			<label className="bold-16 whitespace-nowrap cursor-pointer">
